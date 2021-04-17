47a4b4d80591a3eef3eda370fe8cd2a7c1959b4d
soumen81 <soumen@autumntechcreation.com>
2021-04-17 14:30:08 +0530

    Registration initial

--- app/src/main/java/com/autumntechcreation/click4panditcustomer/ui/register/RegisterActivity.java
+++ app/src/main/java/com/autumntechcreation/click4panditcustomer/ui/register/RegisterActivity.java
@@ -44,11 +44,7 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(in);
             }
         });
-
-
-
-
-       /* mRegisterViewModel.getRegisterResult(mActivityRegisterBinding.edtTxtFirstName.getText().toString(),mActivityRegisterBinding.edtTxtLastName.getText().toString(),
+        /* mRegisterViewModel.getRegisterResult(mActivityRegisterBinding.edtTxtFirstName.getText().toString(),mActivityRegisterBinding.edtTxtLastName.getText().toString(),
                 "",mActivityRegisterBinding.edtTxtEmail.getText().toString(),mActivityRegisterBinding.edtTxtMobileNo.getText().toString(),
                 mActivityRegisterBinding.edtTxtPassword.getText().toString(),mActivityRegisterBinding.edtTxtConfirmPassword.getText().toString(),"CUST")
                 .observe(this, this::handlePendingApprovalListing);*/
